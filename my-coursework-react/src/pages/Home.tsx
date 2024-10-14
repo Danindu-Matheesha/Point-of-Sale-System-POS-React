c5cf4f0ea08c7683970d21b8492f3feb0d364745
Danindu-Matheesha <danindu.matheesha2004@gmail.com>
2024-10-14 21:44:16 +0530

    Fix Home Page Error

--- my-coursework-react/src/pages/Home.tsx
+++ my-coursework-react/src/pages/Home.tsx
@@ -1,33 +1,10 @@
-// import { useAuth } from "../context/AuthContext";
-// function Home() {
-
-//   const { logout } = useAuth()
-
-//   return (
-//     <div>
-//         <h1 className="text-2xl text-center">Point of Sale System (POS)</h1>
-//         <button className="bg-gray-800 text-white px-5 py-2 me-3" onClick={logout}>Logout</button>
-//     </div>
-//   )
-// }
-
-// export default Home;
-
 import { useAuth } from "../context/AuthContext";
 
 function Home() {
     const { logout } = useAuth()
 
     const navigation = [
-        { name: 'Item', href: '#' },
-<<<<<<< HEAD
-        { name: 'Category', href: '/categories' },
-=======
-        { name: 'Category', href: '#' },
->>>>>>> c9bd6fe1466e11de3ff858cdb9077b18c5ba8268
-        { name: 'Stock', href: '#' },
-        { name: 'Cart', href: '#' },
-        { name: 'Login', href: '/auth/login' },
+        {name: "Category" , href: "/categories"}
     ]
 
     return (
