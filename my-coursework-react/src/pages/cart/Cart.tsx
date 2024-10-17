c1c65054e89d1607e6c7396b357b9c79d7cf5bca
Danindu-Matheesha <danindu.matheesha2004@gmail.com>
2024-10-17 23:40:36 +0530

    Modified Cart Page

--- my-coursework-react/src/pages/cart/Cart.tsx
+++ my-coursework-react/src/pages/cart/Cart.tsx
@@ -23,17 +23,35 @@ function Order() {
 
     return (
         <div className="container mx-auto pt-5 pb-5">
-            <h1 className="text-3xl font-semibold mb-5">Cart</h1>
+            <div className="container mx-auto pt-5 pb-5 flex items-baseline justify-between border-b border-gray-400 pb-15 pt-24">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900">Cart</h1>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end ">
+                    <a
+                        href="/"
+                        className="text-sm font-semibold leading-6 text-gray-900"
+                    > Back
+                        <span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div>
+            </div>
 
-            <Link to="/cart/create" className="text-blue-500 mb-5 block">Add Cart</Link>
+            <div className="pt-4 pb-4"></div>
+            <Link to="/cart/create" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 clear">Add Cart</Link>
+            <div className="pt-4 pb-4"></div>
+            
+                <h1 className="text-1xl font-bold tracking-tight text-gray-600 broder underline underline-offset-1">Show All Cart</h1>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end ">
+                    
+                </div>
+
+            <div className="pt-4 pb-4"></div>
 
             <table className="w-full border-separate border-spacing-0 border-none text-left">
-                <thead className="bg-slate-200">
+                <thead className="bg-blue-400">
                     <tr>
-                        <th className="w-[80px]">Cart ID</th>
-                        <th className="w-[200px]">Cart Date and Time</th>
+                        <th className="w-[100px]">Cart ID</th>
+                        <th className="w-[300px]">Cart Date and Time</th>
                         <th className="w-[200px]">Total Amount</th>
-                        <th className="w-[200px]">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -42,13 +60,20 @@ function Order() {
                             <tr>
                                 <td>{cart.id}</td>
                                 <td>{new Date(cart.cartDateTime).toLocaleString()}</td>
-                                <td>{cart.totalPrice}</td>
+                                <td>Rs. {cart.totalPrice}</td>
                                 <td></td>
                             </tr>
                         )
                     })}
                 </tbody>
             </table>
+            <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+                    <div
+                        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    />
+                </div>
         </div>
     )
 }
--- my-coursework-react/src/pages/cart/CreateCart.tsx
+++ my-coursework-react/src/pages/cart/CreateCart.tsx
@@ -24,7 +24,7 @@ function CreateCart() {
     const [total, setTotal] = useState<number>(0);
 
     function addItemToCart(item: ItemType) {
-        const updatedCart = [...cartedItems,item ];
+        const updatedCart = [...cartedItems, item];
         setCartedItems(updatedCart);
     }
 
@@ -38,7 +38,7 @@ function CreateCart() {
     const navigate = useNavigate();
 
     async function saveCart() {
-        var itemIds:any = [];
+        var itemIds: any = [];
 
         cartedItems.map(function (item) {
             itemIds.push(item.id);
@@ -59,15 +59,17 @@ function CreateCart() {
         <div>
             <div className="flex">
                 <div className="w-[400px] border-r border-slate-100 p-2">
-                    <span className="text-xl font-semibold text-slate-800 block h-[40px] p-2">Items</span>
+                    <div className="container mx-auto pt-5 pb-5 flex items-baseline justify-between border-b border-gray-400 pb-15 pt-24">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900">All Items</h1>
+                    </div>
 
                     <div className="mt-5">
                         {items.map(function (item) {
                             return (
-                                <div onClick={() => addItemToCart(item)} className="border border-slate-200 rounded-lg p-2 mb-3">
+                                <div onClick={() => addItemToCart(item)} className="text-slate-600 border border-indigo-600 border-purple-300 rounded-full mb-3 p-3 shadow-lg">
                                     <div className="text-lg font-semibold text-slate-800">{item.name}</div>
                                     <div className="text-sm text-slate-400">{item.itemcategory?.name}</div>
-                                    <div className="text-sm text-green-600 text-right">Rs. {item.price}</div>
+                                    <div className="text-sm text-blue-500 text-right">Rs. {item.price}</div>
                                 </div>
                             )
                         })}
@@ -75,10 +77,22 @@ function CreateCart() {
                 </div>
 
                 <div className="p-2 w-full">
-                    <span className="text-xl font-semibold text-slate-800">New Cart</span>
+                    <div className="container mx-auto pt-5 pb-5 flex items-baseline justify-between border-b border-gray-400 pb-15 pt-24">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900">New Cart Add</h1>
+                        <div className="hidden lg:flex lg:flex-1 lg:justify-end ">
+                            <a
+                                href="/cart"
+                                className="text-sm font-semibold leading-6 text-gray-900"
+                            > Back
+                                <span aria-hidden="true">&rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="pt-5 pb-5"></div>
 
                     <table className="w-full border-separate border-spacing-0 border-none text-left">
-                        <thead>
+                        <thead className="bg-blue-400">
                             <tr>
                                 <th>ID</th>
                                 <th>Description</th>
@@ -91,25 +105,33 @@ function CreateCart() {
                                     <tr>
                                         <td className="w-[80px]">{item.id}</td>
                                         <td className="w-[200px]">{item.name}</td>
-                                        <td className="w-[200px] text-right">{item.price}</td>
+                                        <td className="w-[200px] text-right"> Rs. {item.price}</td>
                                     </tr>
                                 )
                             })}
+                            <div className="pt-5 pb-5"></div>
                             <tr>
                                 <td colSpan={2}>
-                                    <strong>Total</strong>
+                                    <strong>Total Price</strong>
                                 </td>
-                                <td className="border-t border-slate-500 text-right">
-                                    <strong>{total}</strong>
+                                <td className="border-t border-slate-500  text-right">
+                                    <strong>Rs. {total}</strong>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
 
                     <div className="mt-5">
-                        <button type="button" className="py-3 px-4 bg-slate-800 text-white rounded-lg hover:bg-slate-950 mb-2 text-sm" onClick={saveCart}>Save Cart</button>
+                        <button type="button" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 clear" onClick={saveCart}>Save Cart</button>
                     </div>
                 </div>
+            </div>
+            <div
+                aria-hidden="true"
+                className="absolute inset-x-1 -top-50 -z-20 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+                <div
+                    className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                />
             </div>
         </div>
     )
